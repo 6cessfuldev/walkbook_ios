8ce01c69e9b623466c7a2c493a6fd850b18524bc
6cessfuldev <6cessfuldev@gmail.com>
2024-07-10 23:07:30 +0900

    UIKit 코드베이스 초기 세팅

--- walkbook/ViewController.swift
+++ walkbook/ViewController.swift
@@ -8,10 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let testLabel: UILabel = {
+        let label = UILabel()
+        label.text = "초기세팅 테스트"
+        label.textColor = .black
+        label.font = .systemFont(ofSize: 15)
+        label.clipsToBounds = true
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(testLabel)
+        testLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            testLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            testLabel.bottomAnchor.constraint(equalTo: view.centerYAnchor, constant: -50)
+        ])
+        view.backgroundColor = .white
     }
 
 
