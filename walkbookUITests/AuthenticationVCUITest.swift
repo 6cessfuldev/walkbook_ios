25117df4820a767be41888b9a713f289ae3f981c
6cessfuldev <6cessfuldev@gmail.com>
2024-07-20 22:15:31 +0900

    Add Test for SignInView

--- walkbookUITests/AuthenticationVCUITest.swift
+++ walkbookUITests/AuthenticationVCUITest.swift
@@ -55,4 +55,28 @@ final class AuthenticationVCUITest: XCTestCase {
         
         XCTAssert(!signUpBtn.isHittable)
     }
+    
+    func test_SignInButton_탭할_경우_SignInView가_보이는지() throws {
+        let signInBtn = app.buttons["signInButton"]
+        signInBtn.tap()
+        
+        let signInView = app.otherElements["signInView"]
+        XCTAssertTrue(signInView.exists, "SignInView가 표시되지 않습니다.")
+        
+        let googleSignInButton = app.buttons["googleSignInButton"]
+        XCTAssertTrue(googleSignInButton.exists, "구글 로그인 버튼이 표시되야 합니다.")
+        XCTAssertTrue(googleSignInButton.isHittable, "구글 로그인 버튼을 누를 수 없습니다.")
+        
+        let kakaoSignInButton = app.buttons["kakaoSignInButton"]
+        XCTAssertTrue(kakaoSignInButton.exists, "카카오 로그인 버튼이 표시되어야 합니다.")
+        XCTAssertTrue(kakaoSignInButton.isHittable, "카카오 로그인 버튼을 누를 수 없습니다.")
+        
+        let naverSignInButton = app.buttons["naverSignInButton"]
+        XCTAssertTrue(naverSignInButton.exists, "네이버 로그인 버튼이 표시되어야 합니다.")
+        XCTAssertTrue(naverSignInButton.isHittable, "네이버 로그인 버튼을 누를 수 없습니다.")
+        
+        let appleSignInButton = app.buttons["appleSignInButton"]
+        XCTAssertTrue(appleSignInButton.exists, "애플 로그인 버튼이 표시되어야 합니다.")
+        XCTAssertTrue(appleSignInButton.isHittable, "애플 로그인 버튼을 누를 수 없습니다.")
+    }
 }
