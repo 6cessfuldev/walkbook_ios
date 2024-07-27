e3a4381ae33ffca4ca42bda351afc26ab318eb2e
6cessfuldev <6cessfuldev@gmail.com>
2024-07-27 16:05:15 +0900

    Add signInWithGoogle Method in Repository

--- Data/Repositories/FirebaseAuthenticationRepositoryImpl.swift
+++ Data/Repositories/FirebaseAuthenticationRepositoryImpl.swift
@@ -1,7 +1,9 @@
 import Foundation
 import AuthenticationServices
 import FirebaseAuth
+import FirebaseCore
 import CryptoKit
+import GoogleSignIn
 
 class FirebaseAuthenticationRepositoryImpl: NSObject, AuthenticationRepository {
     
@@ -31,6 +33,67 @@ class FirebaseAuthenticationRepositoryImpl: NSObject, AuthenticationRepository {
             String(format: "%02x", $0)
         }.joined()
         return hashString
+    }
+    
+    func signInWithGoogle(presenting viewController: UIViewController, completion: @escaping (Result<String, Error>) -> Void) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else {
+            completion(.failure(NSError(domain: "GoogleLoginError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Missing client ID"])))
+            return
+        }
+        
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: viewController) { [unowned self] result, error in
+            guard error == nil else {
+                completion(.failure(error!))
+                return
+            }
+
+            guard 
+                let user = result?.user, let idToken = user.idToken?.tokenString
+            else {
+                completion(.failure(NSError(domain: "GoogleLoginError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Missing authentication"])))
+                return
+            }
+
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                         accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    self.completion?(.failure(error))
+                    return
+                }
+                
+                if let user = Auth.auth().currentUser {
+                    let uid = user.uid
+                    let email = user.email
+                    let displayName = user.displayName
+                    let photoURL = user.photoURL
+                    let emailVerified = user.isEmailVerified
+                    
+                    // Print user info
+                    print("User ID: \(uid)")
+                    print("User Email: \(String(describing: email))")
+                    print("Display Name: \(String(describing: displayName))")
+                    print("Photo URL: \(String(describing: photoURL))")
+                    print("Email Verified: \(emailVerified)")
+                    self.completion?(.success(email ?? ""))
+                }
+                    
+//                if let error = error {
+//                    completion(.failure(error))
+//                    return
+//                }
+//                
+//                if let authResult = authResult {
+//                    completion(.success(authResult))
+//                } else {
+//                    completion(.failure(NSError(domain: "GoogleLoginError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Unknown error"])))
+//                }
+            }
+        }
     }
 }
 
--- Domain/Interfaces/Repositories/AuthenticationRepository.swift
+++ Domain/Interfaces/Repositories/AuthenticationRepository.swift
@@ -1,3 +1,6 @@
+import UIKit
+
 protocol AuthenticationRepository {
     func signInWithApple(nonce: String, completion: @escaping (Result<String, Error>) -> Void)
+    func signInWithGoogle(presenting viewController: UIViewController, completion: @escaping (Result<String, Error>) -> Void)
 }
